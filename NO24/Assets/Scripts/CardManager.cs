338db5cbfbe1451b61a3c8e42e98f1810ff68d37
shingugitvr000 <shingugitvr000@gmail.com>
2024-04-23 00:09:48 +0900

    [Feat] 카드 틀 생성

--- NO24/Assets/Scripts/CardManager.cs
+++ NO24/Assets/Scripts/CardManager.cs
@@ -2,18 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CardSystem : MonoBehaviour
+public class CardManager : MonoBehaviour
 {
-    public int gameTurn;
-    public int gameMana;
-
-    public enum GAMESTATE
-    {
-        MAINDRAW,
-        PLAYCARD,
-        USECARD,
-    }
-
     // Start is called before the first frame update
     void Start()
     {
